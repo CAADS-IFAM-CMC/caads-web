4ce00c7198c20e1eb1b373368ce48638c171484e
Ademar Castro <ademar.castro.fh@gmail.com>
2023-10-21 10:36:25 -0400

    [FEAT] - Componente menu-header e footer criados. Modulo home criado.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { MenuHeaderComponent } from './components/menu-header/menu-header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+
+// ROTAS FORAM CRIADAS PARA FINS DE DESENVOLVIMENTO EM EQUIPE, POSTERIORMENTE SERÃO DESCARTADAS
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'home', // Define uma rota padrão (se necessário)
     pathMatch: 'full'
   },
+  // Página principal
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  }
+    path: 'home',
+    component: HomeComponent
+  },
+  // Rota para o módulo "menu-header"
+  {
+    path: 'menu-header',
+    component: MenuHeaderComponent
+  },
+  // Rota para o módulo "footer"
+  {
+    path: 'footer',
+    component: FooterComponent
+  },
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,23 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { MenuHeaderComponent } from './components/menu-header/menu-header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HomeModule } from './pages/home/home.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  declarations: [
+    AppComponent,
+    MenuHeaderComponent,
+    FooterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HomeModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
