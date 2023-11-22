63eb1e6574d3ccb8263386c21cd2ba36e87d612b
Ademar Castro <ademar.castro.fh@gmail.com>
2023-11-21 20:36:39 -0400

    Feature/footer (#9)
    * [FEAT] - Estrutura HTML do componente footer feito
    
    * [FEAT][STYLE] - Alterações foram feitas na estrutura HTML. Introduzi a maior parte da estilização do componente Footer
    
    * [STYLE] - Responsividade do componente Footer concluída. Componente finalizado.

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -3,8 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss'],
+  styleUrls: ['./footer.component.scss']
 })
+
 export class FooterComponent  implements OnInit {
 
   constructor() { }
